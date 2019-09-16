d1907987c61372194c0ca63fb2c9b08d8ce9cd7c
DuncantheeDuncan <root@localhost.localdomain>
2019-09-17 00:44:28 +0200

    does not work for big numbers

--- src/main/java/lowlevel/RowSumOddNumbers.java
+++ src/main/java/lowlevel/RowSumOddNumbers.java
@@ -2,22 +2,30 @@ package lowlevel;
 
 public class RowSumOddNumbers {
 
-    public static int rowSumOddNumbers(int n) {
+    public static int rowSumOddNumbers(int cn) {
 
 // System.out.println(n + " given number");
-        int givenNumber = 42;
-        int oddnumber = givenNumber * 2 -1;// gets you an odd number
+        int n = 3;
+        int oddnumber = n * 2 -1;// gets you an odd number
         System.out.println(oddnumber + " odd number");// gives me an odd number!!
         int z= 0;
-        for (int i = 1;i <=givenNumber;i++){
-      z =+ oddnumber *2 -1 + i++;
+int to =0;
+      z =+ oddnumber  /**2 -1*/ ;
+      for(int i=1; i<=n;i++){
+//          int m = + z;
+          System.out.println(z);
+          z+= 2;
+          to = to+ z;
+//          System.out.println(i);
+      }
 
-//     System.out.println(i +  "loop");
 
-        }
-        System.out.println(z -1   +  " outside loop");
 
-        return z -1;
+        System.out.println(z    + " outside loop");
+        System.out.println(to - oddnumber -1+" total");
+        int xoo = to - oddnumber;
+
+        return xoo -1  ;
     }
 
 
