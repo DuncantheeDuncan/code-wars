efced4b951f82deae1aaeff4867c82c0194a9f29
DuncantheeDuncan <mthembup69@gmail.com>
2019-09-19 18:00:52 +0200

    stil to comvert numbers

---
+++ src/main/java/lowlevel/MixedSum.java
@@ -0,0 +1,26 @@
+package lowlevel;
+
+import java.util.List;
+
+public class MixedSum {
+
+
+    /*
+     * Assume input will be only of Integer o String type
+     */
+    public int sum(List<?> mixed) {
+
+        System.out.println(mixed+ " this is mixed");
+        int ii = 
+
+        int total = 0;
+        for (Object i : mixed){
+            int mixednumbers =0;
+            System.out.println(i);
+
+        }
+
+        System.out.println("The total sum is: "+ total);
+        return -1;
+    }
+}
