d7a81ffe39d50becb5126824a118a86a09fde0af
DuncantheeDuncan <mthembup69@gmail.com>
2019-10-04 10:24:51 +0200

    added cumulative string

---
+++ src/main/java/lowlevel/AccumulString.java
@@ -0,0 +1,13 @@
+package lowlevel;
+
+public class AccumulString {
+
+    public static String accum(String s) {
+
+        System.out.println(s.length());
+
+
+
+        return null;
+    }
+}
---
+++ src/test/java/lowlevel/AccumulStringTest.java
@@ -0,0 +1,20 @@
+package lowlevel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AccumulStringTest {
+    private static void testing(String actual, String expected) {
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test() {
+        System.out.println("Fixed Tests accum");
+        testing(AccumulString.accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+        testing(AccumulString.accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+        testing(AccumulString.accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+        testing(AccumulString.accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+        testing(AccumulString.accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+    }
+}
