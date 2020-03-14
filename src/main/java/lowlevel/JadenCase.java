847329324ad223f08f2ce064594a23b26fb46d77
DuncantheeDuncan <mthembup69@gmail.com>
2020-03-14 23:29:23 +0200

    done Jaden Case

--- src/main/java/lowlevel/JadenCase.java
+++ src/main/java/lowlevel/JadenCase.java
@@ -4,6 +4,19 @@ public class JadenCase {
 
     public String toJadenCase(String phrase) {
 
-        return null;
+        String Jadenword ="",concat;
+
+        if(phrase == null | phrase == ""){
+
+            return null;
+        }else {
+            String[] x = phrase.split(" ");
+            for(int i =0; i<x.length;i++){
+                concat =  x[i];
+                concat = " "+concat.substring(0,1).toUpperCase() + concat.substring(1).toLowerCase();
+                Jadenword+=concat;
+            }
+            return  Jadenword.trim();
+        }
     }
 }
