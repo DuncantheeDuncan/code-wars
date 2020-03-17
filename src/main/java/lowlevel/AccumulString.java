9cd32967094114069710732bd2b74825ce93bb6e
DuncantheeDuncan <mthembup69@gmail.com>
2020-03-17 21:45:32 +0200

    Accumulating String donegit add .

--- src/main/java/lowlevel/AccumulString.java
+++ src/main/java/lowlevel/AccumulString.java
@@ -4,10 +4,18 @@ public class AccumulString {
 
     public static String accum(String s) {
 
-        System.out.println(s.length());
+        String accum = "",accumulated = "";
+        for (int i = 0; i < s.length(); i++) {
+            StringBuilder inc = new StringBuilder();
 
+            for (int j = 0; j <= i; j++) {
+                inc.append(s.charAt(i));
+            }
+            accum = inc.substring(0, 1).toUpperCase() + inc.substring(1).toLowerCase();
 
-
-        return null;
+            if (i < s.length() - 1)/*else*/ accum += "-";
+            accumulated += accum;
+        }
+        return accumulated;
     }
 }
